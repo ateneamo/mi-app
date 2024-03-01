b0d0afc03199282978efcb3dc91e323186148af0
ateneamo <ateneamo@yahoo.com>
2024-02-29 21:35:48 -0600

    mapa con mouse basico

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,25 +1,68 @@
 
 
 // componente
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as THREE from 'three';
+import {Component, ElementRef, OnInit, ViewChild, Input} from '@angular/core';
 import {MapService} from './map.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  standalone: true
+  standalone: true,
+  imports:[FormsModule ]
 })
 export class MapComponent implements OnInit {
 
   @ViewChild('rendererCanvas', {static: true})
   public rendererCanvas!: ElementRef<HTMLCanvasElement>;
 
+   @Input () public x:number=0;
+   @Input () public y:number=0;
+   @Input () public z:number=0;
+   @Input () public colorInput:string="0xff0000";
+   @Input () public ancho:number=1;
+   @Input () public largo:number=1;
+    
   public constructor(private engServ: MapService) {
   }
 
   public ngOnInit(): void {
+    
+
     this.engServ.createScene(this.rendererCanvas);
-    this.engServ.animate();
+    this.engServ.addControls();
+    this.engServ.agregaMapa();
+    
+    //this.engServ.animate();
+    this.engServ.agregaStand(3,0.5,0,0x440000);
+    this.engServ.agregaStand(4,0.5,0,0x000034);
+    this.engServ.agregaStand(3,0.5,1,0x110000);
+    this.engServ.agregaStand(4,0.5,1,0x0000ff);
+    
+    this.engServ.renderer.domElement.addEventListener('mouseenter', this.onMouseEnter.bind(this), false);
+    this.engServ.renderer.domElement.addEventListener('click', this.onMouseClick.bind(this), false);
+    
+    
+
+
   }
+
+
+ public onMouseEnter():void {console.log("doble click");
+}
+  public onMouseClick(event: MouseEvent) {
+    console.log("mouse down: ", event.clientX,event.clientY);
+    this.engServ.puntoInicial(event.clientX,event.clientY);
+  
+  }
+ public createCube():void{
+  
+  this.engServ.agregaStand2(this.x,this.y,this.z,parseInt(this.colorInput,16),this.largo,this.ancho);
+ }
+ public createCubeOnClick(x:number,z:number){
+    
+   //
+ }
 
 }
--- src/app/map/map.service.ts
+++ src/app/map/map.service.ts
@@ -1,18 +1,25 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {ElementRef, Injectable, NgZone, OnDestroy} from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class MapService implements OnDestroy {
   private canvas!: HTMLCanvasElement;
-  private renderer!: THREE.WebGLRenderer;
+  public renderer!: THREE.WebGLRenderer;
   private camera!: THREE.PerspectiveCamera;
   private scene!: THREE.Scene;
   private light!: THREE.AmbientLight;
-
+  private controls!:OrbitControls;
   private cube!: THREE.Mesh;
-
+  private stands:any = [];
   private frameId: number = 0;
-
+  private axesHelper = new THREE.AxesHelper( 20 );
+  public mouse= new THREE.Vector2(0,0);
+  public mouseStartPosition= new THREE.Vector2(0,0);
+  public mouseEndPosition= new THREE.Vector2(0,0);
+public rayCaster = new THREE.Raycaster();
+private geometry!:THREE.PlaneGeometry;
+private mapa!: THREE.Mesh;
   public constructor(private ngZone: NgZone) {
   }
 
@@ -21,42 +28,41 @@ export class MapService implements OnDestroy {
    
   }
 
-  public createScene(canvas: ElementRef<HTMLCanvasElement>): void {
+  public createScene(canvas: ElementRef<HTMLCanvasElement>): void 
+  {
     // The first step is to get the reference of the canvas element from our HTML document
-    this.canvas = canvas.nativeElement;
+        this.canvas = canvas.nativeElement;
 
-    this.renderer = new THREE.WebGLRenderer({
-      canvas: this.canvas,
-      alpha: true,    // transparent background
-      antialias: true // smooth edges
-    });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer = new THREE.WebGLRenderer({
+          canvas: this.canvas,
+          alpha: true,    // transparent background
+          antialias: true // smooth edges
+        });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // create the scene
-    this.scene = new THREE.Scene();
+         
+        this.scene = new THREE.Scene();
 
-    this.camera = new THREE.PerspectiveCamera(
-      75, window.innerWidth / window.innerHeight, 0.1, 1000
-    );
-    this.camera.position.z = 5;
-    this.scene.add(this.camera);
+        this.camera = new THREE.PerspectiveCamera(
+          65, window.innerWidth / window.innerHeight, 1, 400
+        );
+        this.camera.position.z = 5;
+        this.camera.position.y =2;
+        this.camera.position.x=3;
+        this.scene.add(this.camera);
 
-    // soft white light
-    this.light = new THREE.AmbientLight(0x404040);
-    this.light.position.z = 10;
-    this.scene.add(this.light);
+        
+        this.light = new THREE.AmbientLight(0x404040);
+        this.light.position.z = 10;
+        this.scene.add(this.light);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
-    this.renderer.render(this.scene, this.camera);
-    console.log('fin crear escena');
+        this.scene.add( this.axesHelper );
+        this.renderer.render(this.scene, this.camera);
+        console.log('fin crear escena');
   }
 
   public animate(): void {
-    // We have to run this outside angular zones,
-    // because it could trigger heavy changeDetection cycles.
+     
     console.log('entrando animar');
     this.ngZone.runOutsideAngular(() => {
       if (document.readyState !== 'loading') {
@@ -74,12 +80,7 @@ export class MapService implements OnDestroy {
   }
 
   public render(): void {
-    this.frameId = requestAnimationFrame(() => {
-      this.render();
-    });
-
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+     
     this.renderer.render(this.scene, this.camera);
   }
 
@@ -88,8 +89,91 @@ export class MapService implements OnDestroy {
     const height = window.innerHeight;
 
     this.camera.aspect = width / height;
+    
     this.camera.updateProjectionMatrix();
 
     this.renderer.setSize(width, height);
   }
+
+  public agregaMapa(){
+   
+    const material = new THREE.MeshBasicMaterial({color:0x0000F1});
+
+    var texture = new THREE.TextureLoader().load('./assets/img/plano.png');
+    var img = new THREE.MeshBasicMaterial
+                  ({
+                  map: texture
+                  });
+
+    this.geometry = new THREE.PlaneGeometry( 50, 50 );
+    this.mapa = new THREE.Mesh( this.geometry, img );
+    this.mapa.name="mapa";
+    this.mapa.rotation.x = - Math.PI / 2;
+    this.mapa.position.y = 0;
+    this.scene.add( this.mapa );
+    this.renderer.render(this.scene, this.camera);
+    console.log(this.mapa);
+    }
+
+  public agregaStand(x:number, y:number,z:number, color1:number){
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({color: color1});
+    const temp: THREE.Mesh=new THREE.Mesh(geometry, material);
+    this.stands.push(temp);
+    temp.position.set(x,y,z);
+    this.scene.add(temp);
+    console.log("agregando cubo:",x, y ,z,color1);
+    this.renderer.render(this.scene, this.camera);
+    console.log(this.stands);
+  }
+  public agregaStand2(x:number, y:number,z:number, color1:number, largo:number, ancho:number){
+    const geometry = new THREE.BoxGeometry(ancho, largo,1);
+    const material = new THREE.MeshBasicMaterial({color: color1});
+   
+    const temp: THREE.Mesh=new THREE.Mesh(geometry, material);
+    this.stands.push(temp);
+    temp.position.set(x,y,z);
+    this.scene.add(temp);
+    console.log("agregando cubo:",x, y ,z,color1);
+    this.renderer.render(this.scene, this.camera);
+    console.log(this.stands);
+  }
+
+  public addControls()
+  {
+      this.controls=new OrbitControls(this.camera, this.renderer.domElement);
+      this.controls.enableDamping = true;
+      this.controls.dampingFactor = 0.25;
+      this.controls.enableZoom = true;
+      this.controls.enablePan = false;
+      this.controls.addEventListener('change', this.render.bind(this)) ;
+      console.log("se agregaron controles");
+
+  }
+
+  
+  public puntoInicial(x:number, y:number)
+  {
+    this.mouse.x = x;
+    this.mouse.y = y;
+    console.log("inicio stand",this.mouse);
+    this.rayCaster.setFromCamera(this.mouse, this.camera);
+    const intersects= this.rayCaster.intersectObjects(this.scene.children);
+    for(let i=0; i<intersects.length;i++)
+    {
+        if (intersects[i].object.name==="mapa")
+        {
+          this.mouseStartPosition.x=intersects[i].point.x;
+          this.mouseStartPosition.y=intersects[i].point.y;
+          console.log("inicio stand",this.mouseStartPosition);
+        }
+    }
+  }
+
+  
+   public dibujaStand(x:number, y:number):void{
+      console.log(x,y);
+    //agregaStand2(x:number, y:number,z:number, 0xCEC9C8, largo:number, ancho:number)
+   } 
+    
 }
